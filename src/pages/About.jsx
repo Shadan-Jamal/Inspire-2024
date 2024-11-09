741c4085723fc6a689d94b4e948fbb9a8e247518
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-11-09 12:42:18 +0530

    changed styling for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,7 @@ function About() {
 
     await animate(
       scope.current,
-      { rotate: 90, width: `${window.innerWidth < 600 ? '50%' : '25%'}`, height : 1, padding : 0},
+      { rotate: 90, width: `${window.innerWidth < 600 ? '100%' : '25%'}`, height : 1, padding : 0},
       { delay : 0.1, duration: 0.7, ease: 'circOut'}
     );
 
@@ -42,13 +42,13 @@ function About() {
       exit={{scale : 0}}
       transition={{duration : 0.5, type : 'keyframes'}}
       className="w-full h-fit text-center row-span-1 flex flex-col justify-center items-center mt-20 ">
-        <h1 className="header-font text-red-600 text-[50px] lg:text-[70px]">About</h1>
+        <h1 className="header-font text-red-600 text-[30px] md:text-[70px]">About</h1>
         <motion.div
         initial={{width:0}}
         animate={{width : '100%'}}
         transition={{duration : 2, type: 'tween' , ease:'circInOut'}}
         className='w-full flex flex-row justify-center gap-5 shadow-2xl shadow-white'>
-          <motion.span className='bg-red-700 h-[1px] w-[300px] lg:w-[500px] rounded-lg'></motion.span>
+          <motion.span className='bg-red-700 h-[1px] w-[200px] lg:w-[500px] rounded-lg'></motion.span>
         </motion.div>
       </motion.div>
 
@@ -63,20 +63,16 @@ function About() {
               exit={{x : '-300vw'}}
               className="w-1/2 h-fit flex justify-end"
               >
-              <img src="/Images/11.jpg" className="w-[450px] h-[400px] object-contain" alt="" />
+              <img src="/Images/11.jpg" className="w-[250px] h-[200px] md:w-[350px] md:h-fit object-contain" alt="" />
             </motion.div>
             <motion.div
             id="para-1" 
             initial={{ x: -100, opacity: 0 }} 
             exit={{x : '300vw' , transition : {ease  : 'circInOut'}}}
             className="w-1/2 h-fit">
-              <motion.p className="text-pretty leading-4 lg:leading-7 text-slate-500 text-xl lg:text-[3vw] text-start lg:p-3 para-font">
+              <motion.p className="text-pretty leading-6 md:leading-7 text-slate-500 text-xl text-[34px] md:text-[2.5vw] text-start lg:p-3 para-font">
                 The Computer Science Department is proud to present Inspire 2k24, an exciting intra-departmental
                 fest designed to ignite creativity, foster innovation, and challenge the technical wits of students. Inspire isn't just a festival—it’s a platform for growth, experimentation, and skill development, where students can step beyond the classroom to experience the thrill of competitive learning.
-                <br />
-                Packed with innovative events, Inspire offers participants the chance to explore the latest trends in
-                technology, hone their problem-solving skills, and engage in hands-on challenges that push them to think
-                outside the box.
               </motion.p>
             </motion.div>
           </motion.div>
@@ -94,10 +90,10 @@ function About() {
         </motion.span>
 
         {/* Right Content Section */}
-        {click && (<motion.div ref={box2} className="w-full h-1/2 flex flex-row justify-end items-center gap-12 px-3 lg:px-10 "> 
+        {click && (<motion.div ref={box2} className="w-full h-1/2 flex flex-row justify-end items-center gap-12 px-3 md:px-10 "> 
             <motion.div initial={{ x: 100, opacity: 0 }} exit={{x : '-300vw'}} id="para-2" className="w-1/2 h-fit">
-              <motion.p className="text-balance leading-4 lg:leading-8 text-slate-500 text-xl lg:text-[3vw] text-end lg:p-3 para-font">
-                Inspire 2k24 is more than just a competition—it’s an incubator for ideas and an opportunity to build connections with other budding technologists. Every event is carefully curated to test critical thinking, analytical abilities, and technical skills, ensuring that participants leave with not just trophies but valuable insights and a refined skillset.
+              <motion.p className="text-balance leading-6 md:leading-8 text-slate-500 text-xl text-[34px] md:text-[2.5vw] text-end lg:p-3 para-font">
+                Events is carefully curated to test technical skills, innovative thinking, ensuring that participants leave with not just trophies but valuable insights and a refined skillset.
                 <br />
                 So gear up for an exhilarating experience at Inspire 2k24—a festival that celebrates the spirit of
                 technology, challenges your limits, and prepares you for the future.
@@ -110,7 +106,7 @@ function About() {
               id="img-2"
               className="w-1/2 h-fit flex justify-start"
             >
-              <img src="/Images/9.jpg" className="w-[450px] h-[400px] object-contain" alt="" />
+              <img src="/Images/9.jpg" className="w-[250px] h-[200px] md:w-[350px] md:h-fit object-contain" alt="" />
             </motion.div>
           </motion.div>
         )}
